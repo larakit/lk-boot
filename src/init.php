b45beb9ab502e62d687a145f770566d8fd4cea98
Alexey Berdnikov <aberdnikov@gmail.com>
2016-05-19 11:35:32 +0500

    fixed

--- src/init.php
+++ src/init.php
@@ -28,5 +28,5 @@ Boot::register_middleware_group('web', [
     \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
     \Illuminate\Session\Middleware\StartSession::class,
     \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-    \App\Http\Middleware\VerifyCsrfToken::class,
+//    \App\Http\Middleware\VerifyCsrfToken::class,
 ]);
