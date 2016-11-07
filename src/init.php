37436a068acad702b676646d02db952ce2ed6dd5
Alexey Berdnikov <aberdnikov@gmail.com>
2016-11-07 15:31:52 +0500

    fixed

--- src/init.php
+++ src/init.php
@@ -14,22 +14,22 @@ Boot::register_middleware(\Illuminate\Foundation\Http\Middleware\CheckForMainten
 /*################################################################################
   route middlewares
 ################################################################################*/
-Boot::register_middleware_route('auth', \App\Http\Middleware\Authenticate::class);
-Boot::register_middleware_route('auth.basic', \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class);
-Boot::register_middleware_route('guest', \App\Http\Middleware\RedirectIfAuthenticated::class);
-Boot::register_middleware_route('throttle', \Illuminate\Routing\Middleware\ThrottleRequests::class);
+//Boot::register_middleware_route('auth', \App\Http\Middleware\Authenticate::class);
+//Boot::register_middleware_route('auth.basic', \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class);
+//Boot::register_middleware_route('guest', \App\Http\Middleware\RedirectIfAuthenticated::class);
+//Boot::register_middleware_route('throttle', \Illuminate\Routing\Middleware\ThrottleRequests::class);
 
 /*################################################################################
   group middlewares
 ################################################################################*/
-Boot::register_middleware_group('api', 'throttle:60,1');
-Boot::register_middleware_group('web', [
-    \App\Http\Middleware\EncryptCookies::class,
-    \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-    \Illuminate\Session\Middleware\StartSession::class,
-    \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-    \App\Http\Middleware\VerifyCsrfToken::class,
-]);
+//Boot::register_middleware_group('api', 'throttle:60,1');
+//Boot::register_middleware_group('web', [
+//    \App\Http\Middleware\EncryptCookies::class,
+//    \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+//    \Illuminate\Session\Middleware\StartSession::class,
+//    \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+//    \App\Http\Middleware\VerifyCsrfToken::class,
+//]);
 
 if(!function_exists('lk_snake')) {
     function lk_snake($value, $delimiter = '_') {
